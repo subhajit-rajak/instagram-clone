311beecaa63967e98e34b2d26436dc46f13e02cb
Subhajit Rajak <subhajitrajak2001@gmail.com>
2024-07-06 05:04:56 +0530

    Feature add - My reels section is profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation ("com.squareup.picasso:picasso:2.8")
     implementation ("com.makeramen:roundedimageview:2.3.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/subhajitrajak/instagramclone/fragments/MyReels.kt
+++ app/src/main/java/com/subhajitrajak/instagramclone/fragments/MyReels.kt
@@ -5,56 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.toObject
+import com.google.firebase.ktx.Firebase
+import com.subhajitrajak.instagramclone.Models.Post
+import com.subhajitrajak.instagramclone.Models.Reel
 import com.subhajitrajak.instagramclone.R
+import com.subhajitrajak.instagramclone.adapters.MyPostRvAdapter
+import com.subhajitrajak.instagramclone.adapters.MyReelAdapter
+import com.subhajitrajak.instagramclone.databinding.FragmentMyReelsBinding
+import com.subhajitrajak.instagramclone.utils.REEL
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyReels.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyReels : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var binding: FragmentMyReelsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_reels, container, false)
+        binding= FragmentMyReelsBinding.inflate(inflater, container, false)
+        var reelList=ArrayList<Reel>()
+        var adapter = MyReelAdapter(requireContext(), reelList)
+        binding.rv.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
+        binding.rv.adapter = adapter
+
+        Firebase.firestore.collection(Firebase.auth.currentUser!!.uid+ REEL).get().addOnSuccessListener {
+            var tempList = arrayListOf<Reel>()
+            for (i in it.documents) {
+                var reel: Reel =i.toObject<Reel>()!!
+                tempList.add(reel)
+            }
+            reelList.addAll(tempList)
+            adapter.notifyDataSetChanged()
+        }
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyReels.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyReels().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
